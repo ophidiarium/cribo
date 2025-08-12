9ef6bcc0be3fbed90558e42c2bda8533c39bb62d
Konstantin Vyatkin <tino@vtkn.io>
2025-08-13 00:21:15 +0200

    chore: cleanup

--- debug_export_check.py
+++
@@ -1,6 +0,0 @@
-"""Check if __version__ should be exported"""
-
-# __version__ doesn't start with underscore (except the dunder)
-# So it should be exported according to Python's default visibility rules
-print(f"__version__ starts with underscore: {'__version__'.startswith('_')}")
-print(f"__version__ is __all__: {'__version__' == '__all__'}")
